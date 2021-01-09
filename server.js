d47ee6ea8666544ee611afa2032680b6f6085e5c
abiancu <alejandrobiancucci@gmail.com>
2021-01-08 22:09:12 -0600

    using express and ejs templates to render the game

--- server.js
+++ server.js
@@ -4,11 +4,17 @@ const app = express();
 
 const PORT = 8080;
 
-app.use(express.static(path.join(__dirname)));
+// const pahserFile = app.use(express.static(path.join(__dirname, )));
+
+// View engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './views'));
 
 app.get('/', (req, res) => {
-  err ? console.log(err) : res.send('index');
+  res.render('index');
 });
+
+// app.use(pahserFile);
 
 app.listen(PORT, () => {
   console.log(`listening on port: ${PORT}`);
